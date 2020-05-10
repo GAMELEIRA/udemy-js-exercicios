1a2d78b1ad4c3810a3124574fdf64f6a518839c4
GAMELEIRA <gameleira270499@gmail.com>
2020-05-10 14:38:47 -0300

    Criado o modulo exercicio-24.js que apresenta 11 vezes o Hello World na tela

---
+++ exercicio-24.js
@@ -0,0 +1,16 @@
+/**
+ * AUTOR:GABRIEL GAMELEIRA DOS SANTOS 
+ * DATA: 10/05/2020
+ * HORA: 14:35
+ * FINALIDADE DO PROGRAMA: IMPRIME 11 VEZES NA TELA O HELLO WORLD
+ */
+
+let contador = 11;
+
+while (contador != 0) {
+
+    console.log("Wello World!");
+
+    contador--;
+
+}
