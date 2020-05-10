7b38fdf3c6f38ebc9e4b2650f3f2c5d2a21c37d2
GAMELEIRA <gameleira270499@gmail.com>
2020-05-10 14:10:16 -0300

    Criado o modulo exercicio-15.js que simula a compra de um carro

---
+++ exercicio-15.js
@@ -0,0 +1,38 @@
+/**
+ * AUTOR:GABRIEL GAMELEIRA DOS SANTOS 
+ * DATA: 10/05/2020
+ * HORA: 14:00
+ * FINALIDADE DO PROGRAMA: VERIFICA SE EXISTE UMA FRUTA NO HORTIFRUT
+ */
+
+const comprarCarro = (carro) => {
+
+    switch (carro) {
+        case 'hatch':
+            console.log('Compra efetuada com sucesso');
+            break;
+
+        case 'sedan':
+
+        case 'motocicleta':
+
+        case 'caminhonete':
+            console.log('Têm certeza que não prefere este modelo?');
+            break;
+
+        default:
+            console.log("Não trabalhamos com este tipo de automóvel aqui");
+            break;
+    }
+
+}
+
+comprarCarro('hatch');
+
+comprarCarro('sedan');
+
+comprarCarro('motocicleta');
+
+comprarCarro('caminhonete');
+
+comprarCarro('ford');
